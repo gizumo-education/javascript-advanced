ac1cd24fa6179df49a448dfa1296f54d2b0b769f
keisuke3 <ishii@gizumo-inc.jp>
2022-11-10 16:48:32 +0900

    スプレッド構文練習問題追加

---
+++ src/lesson/spreadSyntax.js
@@ -0,0 +1,21 @@
+// 問題 1
+const packageManager = ['npm', 'yarn', 'pnpm'];
+
+function arrayFn(array) {
+  const framework = ['Vue', 'React', 'Svelte'];
+}
+
+console.log(arrayFn(packageManager));
+
+// 問題 2
+const objectA = {
+  language: 'JavaScript',
+  framework: 'Vue',
+  version: 2,
+};
+
+const objectB = {
+  version: 3,
+};
+
+console.log();
