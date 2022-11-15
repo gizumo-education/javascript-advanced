59aba75f7a8486fdac1ff5acd31ece2c28a464b6
keisuke3 <ishii@gizumo-inc.jp>
2022-11-15 11:37:04 +0900

    分割代入練習問題作成

---
+++ src/lesson/splitAssignment.js
@@ -0,0 +1,16 @@
+// 問題 1
+function arrayFn(arg) {
+  return ['jQuery', 'Vue', arg];
+}
+
+// 問題 2
+function objectFn() {
+  console.log();
+}
+
+const argObject = {
+  name: 'Gizumo',
+  place: 'Shibuya',
+};
+
+objectFn(argObject);
