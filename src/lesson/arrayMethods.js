744f3799feed753765da79d812b489da511c5b29
keisuke3 <ishii@gizumo-inc.jp>
2022-11-15 11:38:07 +0900

    Arrayメソッド練習問題作成

---
+++ src/lesson/arrayMethods.js
@@ -0,0 +1,15 @@
+// 問題 1
+const array = [
+  {
+    tag: 'p',
+    className: 'hoge',
+  },
+  {
+    tag: 'div',
+    className: 'fuga',
+  },
+  {
+    tag: 'h1',
+    className: 'piyo',
+  },
+];
