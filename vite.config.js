ddbfc6cf8515afc9e756b650556e83e285b3d8cf
keisuke3 <ishii@gizumo-inc.jp>
2022-11-24 19:02:44 +0900

    DOM操作練習問題作成

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,34 @@
+import { resolve } from 'path';
 import { defineConfig } from 'vite';
 import legacy from '@vitejs/plugin-legacy';
 
-export default defineConfig({
-  root: 'src/practice',
-  build: {
-    outDir: '/dist',
-    emptyOutDir: true,
-  },
-  plugins: [
-    legacy({
-      targets: ['ie >= 11'],
-    }),
-  ],
-  server: {
-    open: true,
-    port: 8000,
-  },
+export default (() => {
+  const lessonName = process.env.LESSON_NAME;
+  const devServerPath = lessonName === 'dom' ? 'lesson/domOperation' : lessonName;
+
+  return defineConfig({
+    root: 'src',
+    build: {
+      rollupOptions: {
+        input: {
+          dom: resolve(__dirname, 'src/lesson/domOperation/index.html'),
+          practice: resolve(__dirname, 'src/practice/index.html'),
+        },
+        output: {
+          assetFileNames: '[name]/[ext]/[name].[ext]',
+        }
+      },
+      polyfillModulePreload: false,
+      emptyOutDir: true,
+    },
+    plugins: [
+      legacy({
+        targets: ['ie >= 11'],
+      }),
+    ],
+    server: {
+      open: `/${devServerPath}/index.html`,
+      port: 8000,
+    },
+  });
 });
